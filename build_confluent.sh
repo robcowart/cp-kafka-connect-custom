7bf157fe308ac7f69f6e7113bef65cf75dc0f650
Robert Cowart <robacj@gmail.com>
2020-03-12 17:00:34 +0100

    update to 5.4.1 and various new connectors

--- build_confluent.sh
+++ build_confluent.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #------------------------------------------------------------------------------
 
-docker build --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --squash -f ./confluent.Dockerfile -t robcowart/cp-kafka-connect-custom:5.4.0_1.0.0_confluent .
+docker build --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --squash -f ./confluent.Dockerfile -t robcowart/cp-kafka-connect-custom:5.4.1_1.0.1_confluent .
